d29c200abc107b4c7a46e82ef4bd3f63e61d55aa
RH AGENCY <rhagency07@gmail.com>
2026-04-01 11:44:32 +0700

    Update segment.js

--- netlify/functions/segment.js
+++ netlify/functions/segment.js
@@ -1,1 +1,13 @@
-
+exports.handler = async function (event) {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      message: "Segment function placeholder only. No chunking yet.",
+      step: "segmentation",
+      ready: false
+    })
+  };
+};
